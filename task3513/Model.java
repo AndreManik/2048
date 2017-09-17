a3bcf35fcd3955e02694e7c73c658802aa936544
AndreManik <andre.ivan.manik@gmail.com>
2017-09-17 17:04:33 +0300

    Add files via upload

--- task3513/Model.java
+++ task3513/Model.java
@@ -189,5 +189,21 @@ public class Model {
         return false;
     }
 
+    public void randomMove(){
+        switch (((int) (Math.random() * 100)) % 4){
+            case 0:
+                left();
+                break;
+            case 1:
+                right();
+                break;
+            case 2:
+                up();
+                break;
+            case 3:
+                down();
+                break;
+         }
+    }
 
 }
